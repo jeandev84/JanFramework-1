4f91580fb6d4a397a076969e4cf76927451a375c
Yao Jean-Claude <jeandev84@yahoo.com>
2020-06-16 19:33:37 +0300

    Routing Break Point 1

---
+++ src/Component/Routing/Route.php
@@ -0,0 +1,198 @@
+<?php
+namespace Jan\Component\Routing;
+
+
+use Closure;
+
+
+/**
+ * Class Route
+ * @package Jan\Component\Routing
+*/
+class Route
+{
+
+     /**
+      * @var array
+     */
+     private static $options = [];
+
+
+     /**
+      * @var Router
+     */
+     private static $router;
+
+
+     /**
+      * @return Router
+     */
+     public static function router()
+     {
+        if(! self::$router)
+        {
+            self::$router = new Router();
+        }
+
+        return static::$router;
+     }
+
+
+     /**
+      * @param $methods
+      * @param $path
+      * @param $target
+      * @param null $name
+      * @return Router
+      * @throws Exception\RouterException
+     */
+     public static function map($methods, $path, $target, $name = null)
+     {
+         return self::router()->map(
+             explode('|', $methods), 
+             self::resolvedPath($path), 
+             self::resolvedTarget($target), 
+             $name
+         );
+     }
+
+
+     /**
+      * @param $path
+      * @param $target
+      * @param null $name
+      * @return Router
+      * @throws Exception\RouterException
+     */
+     public static function get($path, $target, $name = null)
+     {
+         return self::map('GET', $path, $target, $name);
+     }
+
+
+     /**
+      * @param $path
+      * @param $target
+      * @param null $name
+      * @return Router
+      * @throws Exception\RouterException
+     */
+     public static function post($path, $target, $name = null)
+     {
+        return self::map('POST', $path, $target, $name);
+     }
+
+
+     /**
+      * @param $path
+      * @param $target
+      * @param null $name
+      * @return Router
+      * @throws Exception\RouterException
+     */
+     public static function put($path, $target, $name = null)
+     {
+        return self::map('PUT', $path, $target, $name);
+     }
+
+
+    /**
+     * @param $path
+     * @param $target
+     * @param null $name
+     * @return Router
+     * @throws Exception\RouterException
+    */
+    public static function delete($path, $target, $name = null)
+    {
+        return self::map('DELETE', $path, $target, $name);
+    }
+
+
+    /**
+     * @param array $options
+     * @param Closure $callback
+    */
+    public static function group(array $options, Closure $callback)
+    {
+        self::$options = $options;
+        $callback();
+        self::$options = [];
+    }
+
+
+    /**
+     * @param $prefix
+     * @param Closure $callback
+    */
+    public static function prefix($prefix, Closure $callback)
+    {
+        self::group(compact('prefix'), $callback);
+    }
+
+
+    /**
+     * @param $namespace
+     * @param Closure $callback
+    */
+    public static function namespace($namespace, Closure $callback)
+    {
+        self::group(compact('namespace'), $callback);
+    }
+
+
+
+    /**
+     * @return array
+    */
+    public static function collections()
+    {
+        return static::router()->routeCollection();
+    }
+
+
+    /**
+     * @param string $path
+     * @return string
+    */
+    private static function resolvedPath(string $path)
+    {
+        if($prefix = self::option('prefix'))
+        {
+            $path = rtrim($prefix, '/') . '/'. ltrim($path, '/');
+        }
+
+        return $path;
+    }
+
+
+    /**
+     * @param string|\Closure $target
+     * @return mixed
+    */
+    private static function resolvedTarget($target)
+    {
+        if(is_string($target) && strpos($target, '@') !== false)
+        {
+            if($namespace = self::option('namespace'))
+            {
+                $target = $namespace .'\\' . $target;
+            }
+
+            list($controller, $action) = explode('@', $target);
+            $target = compact('controller', 'action');
+        }
+
+        return $target;
+    }
+
+
+    /**
+     * @param string $key
+     * @return mixed
+    */
+    private static function option(string $key)
+    {
+        return self::$options[$key] ?? null;
+    }
+}
---
+++ src/Component/Routing/Router.php
@@ -0,0 +1,265 @@
+<?php
+namespace Jan\Component\Routing;
+
+
+use Jan\Component\Routing\Exception\MethodNotAllowedException;
+use Jan\Component\Routing\Exception\RouterException;
+
+
+/**
+ * Class Router
+ * @package Jan\Component\Routing
+*/
+class Router
+{
+
+      /**
+       * @var array
+      */
+      private $route = [];
+
+
+      /**
+       * @var array
+      */
+      private $routes = [];
+
+
+      /**
+       * @var array
+      */
+      private $namedRoutes = [];
+
+
+      /**
+       * @var array
+      */
+      private $patterns = [];
+
+
+      /**
+       * @var array
+      */
+      private $middlewares = [];
+
+
+      /**
+       * Add routes
+       *
+       * @param array $routes
+      */
+      public function setRoutes(array $routes)
+      {
+          $this->routes = $routes;
+      }
+
+
+     /**
+      * Get all routes
+      *
+      * @return array
+     */
+     public function routeCollection()
+     {
+         return $this->routes;
+     }
+
+
+     /**
+      * Get current route
+      *
+      * @return array
+     */
+     public function route()
+     {
+         return $this->route;
+     }
+
+
+     /**
+      * @param array $methods
+      * @param string $path
+      * @param $target
+      * @param string|null $name
+      * @return Router
+      * @throws RouterException
+     */
+     public function map(array $methods, string $path, $target, string $name = null)
+     {
+          $this->routes[] = $this->route = compact('methods', 'path', 'target');
+          $this->setPathName($name, $path);
+          return $this;
+     }
+
+
+     /**
+      * @param $requestMethod
+      * @param $requestUri
+      * @return array|bool
+      * @throws MethodNotAllowedException
+     */
+     public function match(string $requestMethod, string $requestUri)
+     {
+         foreach ($this->routes as $route)
+         {
+             list($methods, $path) = array_values($route);
+
+             if(! \in_array($requestMethod, $methods))
+             {
+                  throw new MethodNotAllowedException();
+             }
+
+             if($params = $this->isMatch($path, $requestUri))
+             {
+                  $route['middleware'] = $this->getMiddleware($path);
+                  return array_merge($route, $params);
+             }
+         }
+
+         return false;
+     }
+
+
+     /**
+      * @param $path
+      * @param $requestUri
+      * @return array|bool
+     */
+     public function isMatch($path, $requestUri)
+     {
+         $pattern = '#^'. trim($path, '/') . '$#i';
+
+         if(preg_match($pattern, $this->getPathFromUrl($requestUri), $matches))
+         {
+             return compact('pattern', 'matches');
+         }
+
+         return false;
+     }
+
+
+    /**
+      * @param array $middleware
+      * @return Router
+     */
+     public function middleware(array $middleware = [])
+     {
+        $this->middlewares[$this->route['path']] = $middleware;
+
+        return $this;
+     }
+
+
+
+     /**
+      * @param string $path
+      * @return array|mixed
+     */
+     public function getMiddleware(string $path)
+     {
+        return $this->middlewares[$path] ?? [];
+     }
+
+
+    /**
+     * @param string $name
+     * @return $this
+     * @throws RouterException
+    */
+    public function name(string $name)
+    {
+        $this->setPathName($name, $this->route['path']);
+        return $this;
+    }
+
+
+
+    /**
+     * @param $name
+     * @param array $params
+     * @return mixed
+    */
+    public function generate($name, array $params = [])
+    {
+        $path = $this->namedRoutes[$name] ?? false;
+
+        if($params)
+        {
+            foreach($params as $k => $v)
+            {
+                /* $path = preg_replace(["#{".$k."}#", "#:". $k ."#"], $v, $path); */
+            }
+        }
+
+        return $path;
+    }
+
+
+    /**
+     * Set regular expression requirement on the route
+     * @param $name
+     * @param null $expression
+     *
+     * @return Router
+     * @throws RouterException
+    */
+    public function where($name, $expression = null)
+    {
+        foreach ($this->parseWhere($name, $expression) as $name => $expression)
+        {
+            if(isset($this->patterns[$name]))
+            {
+                throw new RouterException(sprintf(
+                        'This name (%s) already setted for expression (%s)',
+                        $name, $this->patterns[$name]
+                    )
+                );
+            }
+
+            $this->patterns[$name] = $expression;
+        }
+
+        return $this;
+    }
+
+
+     /**
+      * Determine parses
+      * @param $name
+      * @param $expression
+      * @return array
+     */
+     private function parseWhere($name, $expression)
+     {
+        return \is_array($name) ? $name : [$name => $expression];
+     }
+
+
+    /**
+     * @param string $url
+     * @return string
+     */
+    private function getPathFromUrl(string $url)
+    {
+        return trim(parse_url($url, PHP_URL_PATH), '/');
+    }
+
+
+    /**
+      * @param $name
+      * @param $path
+      * @throws RouterException
+     */
+     private function setPathName($name, $path)
+     {
+         if($name)
+         {
+             if(isset($this->namedRoutes[$name]))
+             {
+                 throw new RouterException('This name (%s) already taken!');
+             }
+
+             $this->namedRoutes[$name] = '/'. trim($path, '/');
+         }
+     }
+}
